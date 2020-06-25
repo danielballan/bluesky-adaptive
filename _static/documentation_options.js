1a624d08770142268f841cd269a3633cc994336e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-06-25 18:40:45 +0000

    Update docs after building Travis build 9 of bluesky/bluesky-adaptive
    The docs were built from the branch 'master' against the commit
    c82d0647188a204c6bb5baf2c4757206a073ffe8.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bluesky/bluesky-adaptive/jobs/702134956.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post11+g524894f',
+    VERSION: '0.post12+gc82d064',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
